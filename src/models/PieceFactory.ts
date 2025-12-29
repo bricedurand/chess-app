ed95e4df2d6d7c8138be0ab3652974cb252e805d
Brice Durand <brice.durand2@gmail.com>
2025-12-29 12:28:47 +0100

    migrate PieceFactory to use new Square design

--- src/models/PieceFactory.ts
+++ src/models/PieceFactory.ts
@@ -1,4 +1,4 @@
-import { Color, SquareNotation } from '../types/chess';
+import { Color } from '../types/chess';
 import { Piece } from './Piece';
 import { Pawn } from './pieces/Pawn';
 import { Rook } from './pieces/Rook';
@@ -6,39 +6,40 @@ import { Knight } from './pieces/Knight';
 import { Bishop } from './pieces/Bishop';
 import { Queen } from './pieces/Queen';
 import { King } from './pieces/King';
+import { Square } from '../utils/Square';
 import { Board } from './Board';
 
 export class PieceFactory {
   // Direct class-based factory methods - no strings!
-  static createPawn(color: Color, square: SquareNotation, board: Board): Pawn {
+  static createPawn(color: Color, square: Square, board: Board): Pawn {
     return new Pawn(color, square, board);
   }
 
-  static createRook(color: Color, square: SquareNotation, board: Board): Rook {
+  static createRook(color: Color, square: Square, board: Board): Rook {
     return new Rook(color, square, board);
   }
 
-  static createKnight(color: Color, square: SquareNotation, board: Board): Knight {
+  static createKnight(color: Color, square: Square, board: Board): Knight {
     return new Knight(color, square, board);
   }
 
-  static createBishop(color: Color, square: SquareNotation, board: Board): Bishop {
+  static createBishop(color: Color, square: Square, board: Board): Bishop {
     return new Bishop(color, square, board);
   }
 
-  static createQueen(color: Color, square: SquareNotation, board: Board): Queen {
+  static createQueen(color: Color, square: Square, board: Board): Queen {
     return new Queen(color, square, board);
   }
 
-  static createKing(color: Color, square: SquareNotation, board: Board): King {
+  static createKing(color: Color, square: Square, board: Board): King {
     return new King(color, square, board);
   }
 
   // Generic factory method using class constructors
   static createPiece<P extends Piece>(
-    PieceClass: new (color: Color, square: SquareNotation, board: Board) => P,
+    PieceClass: new (color: Color, square: Square, board: Board) => P,
     color: Color,
-    square: SquareNotation,
+    square: Square,
     board: Board
   ): P {
     return new PieceClass(color, square, board);
